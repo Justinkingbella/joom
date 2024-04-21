8bb96387d51b0ddd44ed35eb4faa499aaaf9ab4f
Justinkingbella <56661001+Justinkingbella@users.noreply.github.com>
2024-04-21 15:14:12 +0200

    Update MeetingRoom.tsx

--- components/ui/MeetingRoom.tsx
+++ components/ui/MeetingRoom.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils'
-import {  CallControls, CallParticipantsList, CallStatsButton, CallingState, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from '@stream-io/video-react-sdk'
+import {  CallControls, CallParticipantsList, CallStatsButton, CallingState, PaginatedGridLayout, ScreenShareButton, SpeakerLayout, useCallStateHooks } from '@stream-io/video-react-sdk'
 import React, { useState } from 'react'
+
 
 import {
     DropdownMenu,
@@ -15,13 +16,18 @@ import { useRouter, useSearchParams } from 'next/navigation'
 import EndCallButton from './EndCallButton'
 import Loader from './Loader'
 
-type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right'
+type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right' | 'speaker-bottom'
 
 const MeetingRoom = () => {
     const searchParams = useSearchParams();
     const isPersonalRoom = !!searchParams.get ('personal')
     const router = useRouter();
     const [layout, setLayout] = useState<CallLayoutType>('speaker-left')
+    
+
+    const { useScreenShareState } = useCallStateHooks();
+    const { screenShare } = useScreenShareState();
+        
 
     const [showParticipants, setShowParticipants] = useState(false)
     const {useCallCallingState } = useCallStateHooks ();
@@ -35,6 +41,8 @@ const MeetingRoom = () => {
                 return <PaginatedGridLayout/>
             case 'speaker-right':
                 return <SpeakerLayout participantsBarPosition='left'/>
+            case 'speaker-bottom':
+                return <SpeakerLayout participantsBarPosition='top' />
 
             default:
                 return <SpeakerLayout participantsBarPosition='right'/>
@@ -42,11 +50,13 @@ const MeetingRoom = () => {
         }
     }
 
+
   return (
     <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
-        <div className='relative flex size-full items-center justify-center'>
-            <div className='flex size-full max-w-[1000px] items-center'>
-            <CallLayout/>
+        <div className='relative flex size-full items-center justify-center '>
+            <div className='flex flex-auto  size-full max-w-[1000px] items-center justify-items-center '>
+          <CallLayout /> 
+            
             </div>
             <div className={cn('h-[calc(100vh-86px)] hidden ml-2', {'show-block': showParticipants})}>
                 <CallParticipantsList onClose={() =>
@@ -57,14 +67,16 @@ const MeetingRoom = () => {
         </div>
         
 
-        <div className='fixed bottom-0 flex w-ful items-center justify-center gap-5 flex-wrap '>
-        <CallControls onLeave={() => router.push(`/`)}  />
+        <div className='fixed bottom-2 flex  w-full items-center justify-center gap-5 flex-wrap '>
+        <CallControls  onLeave={() => router.push(`/`)}  />
+        
+        
 
         <DropdownMenu>
             <div className='flex items-center'>
 
             <DropdownMenuTrigger className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
-                <LayoutList size={20} className='text-white' />
+                <LayoutList size={17} className='text-white' />
             </DropdownMenuTrigger>
             </div>
             <DropdownMenuContent className='border-dark-1 bg-dark-1 text-white'>
