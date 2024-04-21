8552327ed3616c321c5b693b174a0d34c7caa326
Justinkingbella <56661001+Justinkingbella@users.noreply.github.com>
2024-04-21 15:19:40 +0200

    Update MeetingRoom.tsx

--- components/ui/MeetingRoom.tsx
+++ components/ui/MeetingRoom.tsx
@@ -54,7 +54,7 @@ const MeetingRoom = () => {
   return (
     <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
         <div className='relative flex size-full items-center justify-center '>
-            <div className='flex flex-auto  size-full max-w-[1000px] items-center justify-items-center '>
+            <div className='flex flex-auto  size-full max-w-[1000px] items-center '>
           <CallLayout /> 
             
             </div>
@@ -67,7 +67,7 @@ const MeetingRoom = () => {
         </div>
         
 
-        <div className='fixed bottom-2 flex  w-full items-center justify-center gap-5 flex-wrap '>
+        <div className='fixed bottom-0 flex  w-full items-center justify-center gap-5 flex-wrap '>
         <CallControls  onLeave={() => router.push(`/`)}  />
         
         
